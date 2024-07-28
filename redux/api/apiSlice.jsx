f5436069327fa7b3c29d7c069eecc6256d60dde8
solutionproviderdev <solution.provider.dev@gmail.com>
2024-07-28 18:37:40 +0600

    prepare for build

--- redux/api/apiSlice.jsx
+++ redux/api/apiSlice.jsx
@@ -2,6 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_SOCKET || 'http://192.168.68.108:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl:'http://192.168.68.108:3000' }),
   endpoints: (builder) => ({}),
 });
+
+// baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_SOCKET || 'http://192.168.68.108:3000' }),
