9a186305f79f6d93e86c50bf89a2e7baa36a76fb
solutionproviderdev <solution.provider.dev@gmail.com>
2024-08-02 18:25:24 +0600

    test

--- redux/api/apiSlice.jsx
+++ redux/api/apiSlice.jsx
@@ -1,11 +1,26 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const apiSlice = createApi({
-  reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl:'https://crm.solutionprovider.com.bd/api/' }),
-  endpoints: (builder) => ({}),
-});
+// export const apiSlice = createApi({
+//   reducerPath: "api",
+//   baseQuery: fetchBaseQuery({ baseUrl:'https://crm.solutionprovider.com.bd/api/' }),
+//   endpoints: (builder) => ({}),
+// });
 
 // baseQuery: fetchBaseQuery({ baseUrl:'https://crm.solutionprovider.com.bd/api/' }),
 // baseQuery: fetchBaseQuery({ baseUrl:'http://192.168.68.108:3000' }),
 // baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_SOCKET || 'http://192.168.68.108:3000' }),  
+
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ 
+    baseUrl:'https://crm.solutionprovider.com.bd/api/',
+    prepareHeaders: (headers) => {
+      headers.set('Cache-Control', 'no-cache');
+      headers.set('Pragma', 'no-cache');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({}),
+  refetchOnMountOrArgChange: true,
+});
